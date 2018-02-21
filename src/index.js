4d9d3cf498ec349523064d9005ed0d0a27ce9623
davis009 <davis6247@gmail.com>
2018-02-21 13:03:30 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,84 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expression = "" + initialValue;
+    this.result = 0;
   }
 
-  add(number) {
-    // your implementation
+  copy(smartObj, numObj){ //copy all methods from SmartCalculator to Number object
+      numObj.expression = smartObj.expression;
+      numObj.copy = smartObj.copy;
+      numObj.add = smartObj.add;      
+      numObj.subtract = smartObj.subtract;
+      numObj.multiply = smartObj.multiply;
+      numObj.devide = smartObj.devide;
+      numObj.pow = smartObj.pow;
+  }
+   add(number) {
+      this.expression += "+" + number;
+      
+      this.result = eval(this.expression);
+
+      var numObj = new Number(this.result); 
+      var smartObj = new SmartCalculator(this.expression);   
+      this.copy(smartObj, numObj);  
+      
+      return numObj;    
   }
   
   subtract(number) {
-    // your implementation
+      this.expression += "-" + number;
+
+      this.result = eval(this.expression);
+
+      var numObj = new Number(this.result); 
+      var smartObj = new SmartCalculator(this.expression);
+      this.copy(smartObj, numObj);       
+      
+      return numObj; 
   }
 
   multiply(number) {
-    // your implementation
+    this.expression += "*" + number;
+
+      this.result = eval(this.expression);
+
+      var numObj = new Number(this.result); 
+      var smartObj = new SmartCalculator(this.expression);
+      this.copy(smartObj, numObj);      
+      
+      return numObj;       
   }
 
   devide(number) {
-    // your implementation
+    this.expression += "/" + number;
+
+      this.result = eval(this.expression);
+
+      var numObj = new Number(this.result); 
+      var smartObj = new SmartCalculator(this.expression);
+      this.copy(smartObj, numObj);      
+      
+      return numObj; 
   }
 
-  pow(number) {
-    // your implementation
-  }
+  pow(exponent) {
+      var numbers = this.expression.match(/\d+/g);    //get all number form the expression
+      var previousNumber = numbers[numbers.length - 1];   //get last number
+      this.expression = this.expression.slice(0, this.expression.length - previousNumber.length);
+    
+      previousNumber = Math.pow(+previousNumber, exponent);
+
+      this.expression += previousNumber;
+    
+      this.result = eval(this.expression);
+
+      var numObj = new Number(this.result); 
+      var smartObj = new SmartCalculator(this.expression);
+      this.copy(smartObj, numObj);    
+      
+      return numObj; 
+  }   
 }
 
 module.exports = SmartCalculator;
+
